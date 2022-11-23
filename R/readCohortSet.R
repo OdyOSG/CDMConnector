2fdcb3f18174ad50b10d8eb2d65735ece94d57dc
Adam Black <ablack3@users.noreply.github.com>
2022-11-23 04:59:19 +0000

    Add readCohortSet

---
+++ R/readCohortSet.R
@@ -0,0 +1,34 @@
+
+# Temporarily add this function to CDMConnector
+
+#' Read a set of cohort definitons into R
+#'
+#' A "cohort set" is a collection of cohort definions. In R this is stored in a dataframe with cohortId, cohortName, cohort, and sql columns.
+#' On disk this is stored as a folder with a CohortsToCreate.csv file.
+#'
+#' @param path The path to a folder containing a csv file named CohortsToCreate.csv with columns cohortId, cohortName, and jsonPath.
+#'
+#' @export
+readCohortSet <- function(path) {
+  # cohortsToCreate <- read.csv(file.path(path, "CohortsToCreate.csv"), stringsAsFactors = FALSE) %>%
+  cohortsToCreate <- readr::read_csv(file.path(path, "CohortsToCreate.csv"), show_col_types = FALSE) %>%
+    dplyr::mutate(sql = NA, cohort = purrr::map(jsonPath, jsonlite::read_json))
+
+
+  if (nrow(cohortsToCreate) == 0) return(cohortsToCreate)
+
+  for (i in 1:nrow(cohortsToCreate)) {
+    cohortJson <- readr::read_file(cohortsToCreate$jsonPath[i])
+    cohortDef <- jsonlite::read_json(cohortsToCreate$jsonPath[i]) # change to Capr::readCohort
+    cohortExpression <- CirceR::cohortExpressionFromJson(cohortJson)
+    cohortSql <- CirceR::buildCohortQuery(cohortExpression, options = CirceR::createGenerateOptions(generateStats = FALSE))
+    cohortSql <- SqlRender::render(cohortSql, warnOnMissingParameters = FALSE) # pre-render sql to remove extraneous code
+    # cohortsToCreate$json[i] <- cohortJson # any reason to give the user json text strings?
+    # cohortsToCreate$cohort[i] <- cohortDef # TODO make this a Capr cohort object
+    cohortsToCreate$sql[i] <- cohortSql
+  }
+
+  cohortsToCreate %>%
+    dplyr::select(.data$cohortId, .data$cohortName, .data$cohort, .data$sql)
+}
+
