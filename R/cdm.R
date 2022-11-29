b1edf1839551e2b1f64ea9ab83459378f14900e7
Adam Black <ablack3@users.noreply.github.com>
2022-11-29 15:07:24 -0500

    Take eunomia out of the package and download it separately.

--- R/cdm.R
+++ R/cdm.R
@@ -263,14 +263,46 @@ tbl_group <- function(group) {
 #' }
 eunomia_dir <- function(exdir = NULL) {
   rlang::check_installed("duckdb", version = "0.6.0", reason = "duckdb version 0.6 is required to use eunomia_dir()")
+  if (!eunomia_is_available()) {
+    if (interactive()) {
+      r <- menu(c("Yes", "No"),
+                title = "Eunomia has not been downloaded yet.\nWould you like to download the Eunomia example CDM dataset?")
+    } else {
+      r <- 1 # Automatically try downloading Eunomia if R is running non-interactively.
+    }
+  }
+
+  if (r != 1) rlang::abort("Eunomia dataset is not available")
 
   if (is.null(exdir)) exdir <- file.path(tempdir(TRUE), paste(sample(letters, 8, replace = TRUE), collapse = ""))
-  file <- xzfile(system.file("duckdb", "cdm.duckdb.tar.xz", package = "CDMConnector"), open = "rb")
+  file <- eunomia_cache()
   untar(file, exdir = exdir)
   close(file)
   path <- file.path(exdir, "cdm.duckdb")
   if(!file.exists(path)) rlang::abort("Error creating Eunomia CDM")
-  path
+  return(path)
+}
+
+# has the eunomia dataset been cached?
+eunomia_is_available <- function() {
+  path <- Sys.getenv("EUNOMIA_CACHE_DIR", unset = "~/eunomia_cache")
+  path <- path.expand(path)
+  file.exists(file.path(path, "cdm.duckdb.tar.xz"))
+}
+
+# return the location to the cached eunomia dataset
+eunomia_cache <- function() {
+  path <- Sys.getenv("EUNOMIA_CACHE_DIR", unset = "~/eunomia_cache")
+  path <- path.expand(path)
+  if (!eunomia_is_available()) {
+    if (!dir.exists(path)) dir.create(path)
+
+    download.file("https://github.com/OdyOSG/EunomiaData/raw/main/cdm.duckdb.tar.xz",
+                  file.path(path, "cdm.duckdb.tar.xz"),
+                  method = "auto")
+  }
+  if (!eunomia_is_available()) rlang::abort("Error downloading Eunomia dataset.")
+  return(path)
 }
 
 #' @export
